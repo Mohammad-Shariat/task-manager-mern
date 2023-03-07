44ff4c002ca3a3c604ee7e919bbfd8386625ed1a
Mohammad Shariat <mohamadshariatahmadi@gmail.com>
2023-03-07 22:32:48 +0330

    create routes and connecting to database

---
+++ server/server.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const tasksRoutes = require('./routes/tasks');
+
+// express app
+const app = express();
+
+// middleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// routes
+app.use('/api/tasks', tasksRoutes);
+
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log('connected on db & listening on port: ', process.env.PORT);
+    });
+  })
+  .catch(error => {
+    console.log(error);
+  });
